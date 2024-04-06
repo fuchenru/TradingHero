7767964bb8fe09b85a30fd353bed1123def86cff
chsuehkelvin <chsueh@umich.edu>
2024-04-06 15:22:54 -0500

    add stand

--- get_news.py
+++ get_news.py
@@ -1,6 +1,7 @@
 import finnhub
 from datetime import date
 from textblob import TextBlob
+import pandas as pd
 
 finnhub_client = finnhub.Client(api_key="co6v709r01qj6a5mgco0co6v709r01qj6a5mgcog")
 
@@ -20,8 +21,9 @@ def classify_sentiment(text):
         return 'neutral'
 
 def get_stock_news(ticker_symbol):
-    news = finnhub_client.company_news(ticker_symbol, _from=formatted_today, to=formatted_today)
+    news = finnhub_client.company_news(ticker_symbol, _from="2024-04-01", to="2024-04-01")
     df = pd.DataFrame.from_records(news, columns=['headline', 'summary'])
     top_5_news = df.head(5)
     top_5_news['stand'] = top_5_news['summary'].apply(classify_sentiment)
+    top_5_news = top_5_news.reset_index(drop=True)
     return top_5_news
