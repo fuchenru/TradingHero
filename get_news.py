79fbe4d1cd740fdac32060b26120c352bd6941fc
chsuehkelvin <60193969+chsuehkelvin@users.noreply.github.com>
2024-04-06 00:05:49 -0500

    Update get_news.py
    add today's date in order to get the latest news related to the targeted company

--- get_news.py
+++ get_news.py
@@ -1,5 +1,10 @@
 import finnhub
+from datetime import date
+
 finnhub_client = finnhub.Client(api_key="co6v709r01qj6a5mgco0co6v709r01qj6a5mgcog")
 
+today = date.today()
+formatted_today = today.strftime('%Y-%m-%d')
+
 def get_stock_news(ticker_symbol):
-    return finnhub_client.company_news(ticker_symbol, _from="2024-04-01", to="2024-04-30")[0:5]
+    return finnhub_client.company_news(ticker_symbol, _from=formatted_today, to=formatted_today)[0:5]
