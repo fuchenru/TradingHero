277b4b823ed1475670ecd136071b3165ababc967
fuchenru <fuchenru@msu.edu>
2024-05-02 15:20:22 -0500

    modify docker file

--- Dockerfile
+++ Dockerfile
@@ -11,13 +11,30 @@ ENV APP_HOME /app
 WORKDIR $APP_HOME
 
 # Copy Streamlit app
-ADD streamlit_app.py ./
+ADD app.py ./
 
 # Install dependencies
 RUN python -m pip install --no-cache-dir \
         streamlit==1.33.0 \
-        google-generativeai==0.5.0
+        google-generativeai==0.5.0 \
+        streamlit_plotly_events \
+        yfinance \
+        finnhub-python \
+        pandas \
+        datetime \
+        plotly \
+        numpy \
+        scipy \
+        google.generativeai \
+        prophet \
+        scikit-learn \
+        TextBlob \
+        torch \
+        transformers \
+        chardet \
+        huggingface_hub
+
 
 
 # Start Streamlit app
-ENTRYPOINT ["streamlit", "run", "streamlit_app.py", "--server.port=8080"]
+ENTRYPOINT ["streamlit", "run", "app.py", "--server.port=8080"]
--- streamlit_gcp_sample.sh
+++ streamlit_gcp_sample.sh
@@ -1,4 +1,4 @@
-gcloud config set project PROJECT-ID
+gcloud config set project adsp-capstone-trading-hero
 
 cd DIRECTORY
 
@@ -17,15 +17,15 @@ gcloud artifacts repositories list
 
 
 # Create a repository (if it does  not exist)
-gcloud artifacts repositories create REPOSITORY --repository-format=docker --location=us --description="YOUR_DESCRIPTION"
+gcloud artifacts repositories create tradinghero --repository-format=docker --location=us --description="trading hero test push"
 
 
 # Build Docker image
-docker image build -t us-docker.pkg.dev/PROJECT-ID/REPOSITORY/APP-NAME:latest .
+docker image build -t us-docker.pkg.dev/adsp-capstone-trading-hero/tradinghero/app.py:latest .
 
 
 # Push Docker image into artifact registry
-docker push us-docker.pkg.dev/PROJECT-ID/REPOSITORY/APP-NAME:latest
+docker push us-docker.pkg.dev/adsp-capstone-trading-hero/tradinghero/app.py:latest
 
 
 # Enable  Cloud Run API
@@ -37,14 +37,15 @@ gcloud services enable run.googleapis.com
 # Must enable session affinity (sticky routing between a browser session and a serving container)
 # This is because Streamlit uses WebSocket connections for rendering the app but it uses an HTTP connection for the file uploader widget. 
 # For the app to work correctly, both WebSocket and HTTP connections must be established with the same container instance.
-gcloud run deploy APP-NAME \
- --image=us-docker.pkg.dev/PROJECT-ID/REPOSITORY/APP-NAME:latest \
+gcloud run deploy app.py \
+ --image=us-docker.pkg.dev/adsp-capstone-trading-hero/tradinghero/app.py:latest \
  --platform managed \
  --allow-unauthenticated \
  --region=us-central1 \
- --project=PROJECT-ID \
- --set-env-vars='GEMINI_API_KEY'
+ --project=adsp-capstone-trading-hero \
+#  --set-env-vars='GEMINI_API_KEY'
 
+gcloud run deploy tradinghero --image=us-docker.pkg.dev/adsp-capstone-trading-hero/tradinghero/app.py:latest --platform managed --region us-central1 --allow-unauthenticated --project=adsp-capstone-trading-hero
 
 # Delete Streamlit app when no longer needed
 gcloud run services delete APP-NAME --region=us-central1 --quiet
