81403eaaf7faa7120425d6c963ba2be1c6d225be
Nathan <164469864+nathanchen07@users.noreply.github.com>
2024-05-07 11:30:49 -0500

    predict.py

--- predict.py
+++ predict.py
@@ -5,9 +5,14 @@ from prophet import Prophet
 import yfinance as yf
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 from prophet.plot import plot_plotly, plot_components_plotly
-import google.generativeai as genai
-genai.configure(api_key="AIzaSyDfwWZH59XOxiVnz6XRyXWIX47hF7BZ1jQ")
-model = genai.GenerativeModel(model_name="gemini-pro")
+import vertexai
+from vertexai.preview.generative_models import GenerativeModel, ChatSession, Part
+import vertexai.preview.generative_models as generative_models
+
+
+vertexai.init(project="adsp-capstone-trading-hero", location="us-central1")
+# Define the model for Gemini Pro
+model = GenerativeModel("gemini-1.0-pro")
 
 def transform_price(df):
     df = df[['Adj Close']].reset_index()
@@ -30,7 +35,15 @@ def calculate_performance_metrics(actual, predicted):
     rmse = np.sqrt(mse)
     return {'MAE': mae, 'MSE': mse, 'RMSE': rmse}
 
-def generate_gemini_tsresponse(tsprompt, future_price,metrics_data):
+def extract_text_from_generation_response(responses):
+    """Extracts the concatenated text from the responses and removes extra newlines/spaces."""
+    concatenated_text = []
+    for candidate in responses.candidates:
+        for part in candidate.content.parts:
+            concatenated_text.append(part.text.strip())
+    return concatenated_text[0]
+
+def generate_vertexai_tsresponse(tsprompt, future_price,metrics_data):
     future_price = future_price.to_string()
-    response = model.generate_content([tsprompt, future_price, metrics_data])
-    return response.text
+    responses = model.generate_content([tsprompt, future_price, metrics_data])
+    return extract_text_from_generation_response(responses)
