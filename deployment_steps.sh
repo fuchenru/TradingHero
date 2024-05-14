24640c38d1e3e7a365d2cb9aa7aae0aa8dda0687
fuchenru <fuchenru@msu.edu>
2024-05-13 22:35:07 -0500

    fix deployment steps

--- deployment_steps.sh
+++ deployment_steps.sh
@@ -1,7 +1,6 @@
 gcloud config set project adsp-capstone-trading-hero
 
-cd DIRECTORY
-
+cd TradingHero
 
 
 ## Clean-up (to get fresh experience) and remove all unused and dangling images
@@ -32,20 +31,7 @@ docker push us-docker.pkg.dev/adsp-capstone-trading-hero/tradinghero/app:latest
 gcloud services enable run.googleapis.com
 
 
-
-# Deploy the Streamlit app to Cloud Run
-# Must enable session affinity (sticky routing between a browser session and a serving container)
-# This is because Streamlit uses WebSocket connections for rendering the app but it uses an HTTP connection for the file uploader widget. 
-# For the app to work correctly, both WebSocket and HTTP connections must be established with the same container instance.
-gcloud run deploy app.py \
- --image=us-docker.pkg.dev/adsp-capstone-trading-hero/tradinghero/app.py:latest \
- --platform managed \
- --allow-unauthenticated \
- --region=us-central1 \
- --project=adsp-capstone-trading-hero \
-#  --set-env-vars='GEMINI_API_KEY'
-
-gcloud run deploy tradinghero --image=us-docker.pkg.dev/adsp-capstone-trading-hero/tradinghero/app:latest --platform managed --region us-central1 --allow-unauthenticated
+# Go to Artifact Registry select repository -> app -> the latest image and deploy on cloud run
 
 # Delete Streamlit app when no longer needed
 gcloud run services delete APP-NAME --region=us-central1 --quiet
